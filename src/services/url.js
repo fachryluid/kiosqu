bcad4de4583acac1b26d2e6ebeda7eea7d5da65a
kikiisa <kikiisa89@gmail.com>
2022-10-10 22:11:23 +0800

    composition api

---
+++ src/services/url.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://kiosqu.herokuapp.com/api/'
+
+export default axios
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,56 +1,80 @@
 <template>
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-4">
-        <div class="card border-0">
-          <div class="card-body text-start">
-            <h6><strong>LOGIN TO ACCOUNT</strong></h6>
+    <form @submit.prevent="login">
+        <div class="row justify-content-center">
+        <div class="col-md-4">
+            <div class="card border-0">
+            <div class="card-body text-start">
+                <h6><strong>LOGIN TO ACCOUNT</strong></h6>
+                <hr>
+                <div class="form-group text-start">
+                <label for="user"><strong>Username</strong></label>
+                <div class="input-group mb-3 mt-2">
+                    <span class="input-group-text" id="basic-addon1"
+                    ><i class="fa fa-user"></i
+                    ></span>
+                    <input
+                    v-model="log.username"
+                    type="text"
+                    class="form-control"
+                    placeholder="Username"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    />
+                </div>
+                </div>
+                <div class="form-group text-start">
+                <label for="user"><strong>Password</strong></label>
+                <div class="input-group mb-3 mt-2">
+                    <span class="input-group-text" id="basic-addon1"
+                    ><i class="fa fa-lock"></i
+                    ></span>
+                    <input
+                    v-model="log.password"
+                    type="password"
+                    class="form-control"
+                    placeholder="Password"
+                    aria-label="Password"
+                    aria-describedby="basic-addon1"
+                    />
+                </div>
+                </div>
+                <button class="btn btn-danger w-100 mt-1">
+                LOGIN
+                </button>
+            </div>
+            </div>
+            <p class="text-center mt-3">Need an Account ? <a href="http://">Register</a></p>
             <hr>
-            <div class="form-group text-start">
-              <label for="user"><strong>Email</strong></label>
-              <div class="input-group mb-3 mt-2">
-                <span class="input-group-text" id="basic-addon1"
-                  ><i class="fa fa-user"></i
-                ></span>
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Email Address"
-                  aria-label="Email"
-                  aria-describedby="basic-addon1"
-                />
-              </div>
-            </div>
-            <div class="form-group text-start">
-              <label for="user"><strong>Password</strong></label>
-              <div class="input-group mb-3 mt-2">
-                <span class="input-group-text" id="basic-addon1"
-                  ><i class="fa fa-lock"></i
-                ></span>
-                <input
-                  type="password"
-                  class="form-control"
-                  placeholder="Password"
-                  aria-label="Password"
-                  aria-describedby="basic-addon1"
-                />
-              </div>
-            </div>
-            <button class="btn btn-danger w-100 mt-1">
-              LOGIN
-            </button>
-          </div>
+            <button class="btn btn-light w-100"><i class="fa-brands fa-google ms-3"></i>  Login With Google </button>
         </div>
-        <p class="text-center mt-3">Need an Account ? <a href="http://">Register</a></p>
-        <hr>
-        <button class="btn btn-light w-100"><i class="fa-brands fa-google ms-3"></i>  Login With Google </button>
-      </div>
-    </div>
+        </div>
+    </form>
   </div>
 </template>
 <script>
+import { reactive } from '@vue/reactivity';
+import http from '../services/url'
 export default {
-  setup() {},
+  setup() {
+        const log = reactive({
+            username:"",
+            password:""
+        })
+        const login = async () => 
+        {
+            await http.post('login',{
+                username:log.username,
+                password:log.password
+            })
+            .then((response) => {
+                console.log(response)
+            }).catch((error) => {
+                console.log(error)
+            })
+        }
+        return { login,log }
+  },
 };
 </script>
 <style scoped>
